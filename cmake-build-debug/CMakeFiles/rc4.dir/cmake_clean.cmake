0bb29227048dec060d10a56d8ea6b96bf21c644b
CXIR <cxir.developer@gmail.com>
2018-11-15 16:02:10 +0100

    Initial Commit
    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/rc4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rc4.dir/main.cpp.o"
+  "rc4.pdb"
+  "rc4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rc4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/decipher.h
@@ -0,0 +1,8 @@
+//
+// Created by Ludwig Roger on 24/10/2018.
+//
+
+#ifndef RC4_DECIPHER_H
+#define RC4_DECIPHER_H
+
+#endif //RC4_DECIPHER_H
