9fe9d266db0a2240249d60d30871c0e20ad52958
aimee-elks <aimee.elmkies@outlook.com>
2018-11-28 17:14:44 +0100

    Update RC4.cpp

--- src/RC4.cpp
+++ src/RC4.cpp
@@ -45,10 +45,79 @@ void RC4::ksa() {
 
 }
 
+
 string RC4::prga() {
 
     string result = "";
-    int i = 0;
+    int content_length = this->content.length();
+
+    //THREAD 1
+    int min1 = 0;
+    int max1 = content_length/3;
+
+    //THREAD 2
+    int min2 = max1+1;
+    int max2 = (content_length/3)*2;
+
+    //THREAD 3
+    int min3 = max2+1;
+    int max3 = content_length;
+
+    std::mutex lock;
+    std::thread t1([&lock]() {
+        int i = 0;
+        int j = 0;
+        lock.lock();
+        for(int a = min1; a < max1; a++){
+
+            i = ( i + 1) % this->mod;
+            j = (j + this->S[i]) % this->mod;
+            swap(i, j);
+
+            unsigned char k = S[(S[i] + S[j]) % this->mod] ^ this->content[a];
+            result += k;
+
+        }
+        lock.unlock();
+    });
+    std::thread t2([&lock]() {
+        int i = 0;
+        int j = 0;
+        lock.lock();
+        for(int a = min2; a < max2; a++){
+
+            i = ( i + 1) % this->mod;
+            j = (j + this->S[i]) % this->mod;
+            swap(i, j);
+
+            unsigned char k = S[(S[i] + S[j]) % this->mod] ^ this->content[a];
+            result += k;
+
+        }
+        lock.unlock();
+    });
+    std::thread t3([&lock]() {
+        int i = 0;
+        int j = 0;
+        lock.lock();
+        for(int a = min3; a < max3; a++){
+
+            i = ( i + 1) % this->mod;
+            j = (j + this->S[i]) % this->mod;
+            swap(i, j);
+
+            unsigned char k = S[(S[i] + S[j]) % this->mod] ^ this->content[a];
+            result += k;
+
+        }
+        lock.unlock();
+    });
+    t1.join();
+    t2.join();
+    t3.join();
+
+
+    /*int i = 0;
     int j = 0;
 
     for(int a = 0; a < this->content.length(); a++){
@@ -60,7 +129,9 @@ string RC4::prga() {
         unsigned char k = S[(S[i] + S[j]) % this->mod] ^ this->content[a];
         result += k;
 
-    }
+    }*/
+
+
     return result;
 }
 
